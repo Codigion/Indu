b8e0b1837f55b4b0ee7315645ad2f93763facaee
AnjaniGourisaria <gourisariaanjani@gmail.com>
2024-08-21 01:26:07 +0530

    ...

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,18 +1,33 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
+/* Web View */
+// flutter_webview_pro: ^3.0.1+4
+// Supports: Android File Chooser in Web View
 import 'package:flutter_webview_pro/webview_flutter.dart';
+/* Screen Orientation */
 import 'package:flutter/services.dart';
+/* URL Launcher */
 import 'package:url_launcher/url_launcher.dart';
+/* Permission Handler */
 import 'package:permission_handler/permission_handler.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  /* Set the system UI overlay flags to ensure proper spacing! */
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
+    statusBarColor: Colors.transparent, /* Set Status Bar Transparent */
   ));
+
+  /* Request Camera Permission */
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure WidgetsBinding is initialized.
   final status = await Permission.camera.request();
+  if (status.isGranted) {
+    /* If Granted */
+  } else {
+    /* If Not Granted */
+  }
+
   runApp(const MyApp());
 }
 
@@ -24,97 +39,86 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final Completer<WebViewController> _controller = Completer<WebViewController>();
-  final connectivity = Connectivity();
-  StreamSubscription? connectivitySubscription;
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
 
   @override
   void initState() {
     super.initState();
     if (Platform.isAndroid) WebView.platform = AndroidWebView();
-    checkConnectivity();
-  }
-
-  void checkConnectivity() {
-    connectivitySubscription = connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
-      if (result == ConnectivityResult.none) {
-        // Handle offline
-        _showOfflineScreen();
-      } else {
-        // Handle online
-        if (_controller.isCompleted) {
-          _controller.future.then((controller) {
-            controller.loadUrl('https://cow-identifier.codigion.com/');
-          });
-        }
-      }
-    });
-  }
-
-  void _showOfflineScreen() {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (_) => AlertDialog(
-        title: const Text('Offline'),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/logo.png'),
-            const Text('You are currently offline. Check your connection.')
-          ],
-        ),
-      ),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
+    /* Only Portrait Orientation */
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
+
     return MaterialApp(
+      /* Disable Debug */
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        /* Extra spacing on Top for Notch display! */
         body: SafeArea(
           child: WebView(
-            initialUrl: 'https://cow-identifier.codigion.com/',
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (WebViewController webViewController) {
-              _controller.complete(webViewController);
-            },
-            javascriptChannels: <JavascriptChannel>{
-              _toasterJavascriptChannel(context),
-            },
-            navigationDelegate: (NavigationRequest request) {
-              if (!request.url.startsWith("https://cow-identifier.codigion.com/")) {
-                launchExternalURL(request.url);
-                return NavigationDecision.prevent;
-              }
-              return NavigationDecision.navigate;
-            },
-            gestureNavigationEnabled: true,
-            geolocationEnabled: false,
-          ),
+              initialUrl: 'https://cow-identifier.codigion.com/',
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) async {
+                _controller.complete(webViewController);
+              },
+              onProgress: (int progress) {
+                /* Nothing */
+              },
+              javascriptChannels: <JavascriptChannel>{
+                _toasterJavascriptChannel(context),
+              },
+              navigationDelegate: (NavigationRequest request) {
+                if (!request.url
+                    .startsWith("https://cow-identifier.codigion.com/")) {
+                  launchExternalURL(request.url);
+                  return NavigationDecision.prevent;
+                }
+
+                return NavigationDecision.navigate;
+              },
+              onPageStarted: (String url) {
+                /* Nothing */
+              },
+              onPageFinished: (String url) {
+                /* Nothing */
+              },
+              onWebResourceError: (WebResourceError error) {
+                debugPrint('''
+                      [Page Resource Error]
+                      Code: ${error.errorCode}
+                      Description: ${error.description}
+                      Error Type: ${error.errorType}
+                      ''');
+              },
+              gestureNavigationEnabled: true,
+              geolocationEnabled: false),
         ),
       ),
     );
   }
 
-  @override
-  void dispose() {
-    connectivitySubscription?.cancel();
-    super.dispose();
-  }
-
+  /* Launch External URL in Web View */
   void launchExternalURL(String url) async {
-    if (await canLaunch(url)) {
+    if (url.startsWith('tel:')) {
+      await launch(url);
+    } else if (url.startsWith('mailto:')) {
       await launch(url);
     } else {
-      throw 'Oops! Cannot Open: $url';
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Oops! Cannot Open: $url';
+      }
     }
   }
 
+  /* JavaScript Response as Toast */
   JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
     return JavascriptChannel(
       name: 'Toaster',
